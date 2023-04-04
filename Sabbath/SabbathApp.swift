e4553128fabe8b64d700fb96c1520588b9a2d286
201tangj <tangjf@bc.edu>
2023-04-04 14:07:40 -0400

    Initial Commit

---
+++ Sabbath/SabbathApp.swift
@@ -0,0 +1,17 @@
+//
+//  SabbathApp.swift
+//  Sabbath
+//
+//  Created by Jasmine on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SabbathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
